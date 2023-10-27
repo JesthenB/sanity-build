34c0f401727437ea73b0aeba0d190d1e7c9717f4
JesthenB <jesthenh@gmail.com>
2023-10-27 14:03:29 -0500

    cleaned up some files that had duplicates in the root folder and not in the sanity folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,8 @@ export default function Home() {
           </h2>
         </div>
 
-        <img className="hidden md:inline-flex h-32 lg:h-full"
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt=""
         />
